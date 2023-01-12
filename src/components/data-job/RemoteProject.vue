3f2732d8fb7512e2b5c5c8f5f8931b4f1d3e89a4
dongluyang <dongluyang2005@126.com>
2023-01-12 11:11:36 +0800

    support fetch projects

--- src/components/data-job/RemoteProject.vue
+++ src/components/data-job/RemoteProject.vue
@@ -6,15 +6,14 @@
           role="button"
           aria-controls="contentIdForA11y3">
           <p class="card-header-title">
-            <template v-if="!loading">{{project.title}}</template>
+            <template v-if="!loading">{{project.name}}</template>
             <b-skeleton size="is-large" :active="loading"></b-skeleton>
           </p>
         </div>
         <div class="card-content">
           <div class="content">
             <template v-if="!loading">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-              <a>#buefy</a>.
+              <a>{{project.alias}}</a>.
             </template>
             <b-skeleton size="is-large" :active="loading" :count="2"></b-skeleton>
           </div>
@@ -35,20 +34,42 @@ name: "RemoteProject",
 data(){
   return {
     loading: false,
-    projectList:[{
-      title: 'Title 1',
-      text: 'Text 1'
-    },
-      {
-        title: 'Title 2',
-        text: 'Text 2'
-      },
-      {
-        title: 'Title 3',
-        text: 'Text 3'
-      }]
+    projectList:[]
   }
-}
+},
+  mounted() {
+    this.listProjects()
+  },
+  methods: {
+    listProjects() {
+      this.$http.post("http://newsightcgteam.cgyun.cn:18889/cgteam/cgteam/getAllProjects").then(response=>{
+        let obj = {};
+        let res = response.data
+        let data = res.reduce((cur, next) => {
+          obj[next.project] ? '' : (obj[next.project] = true && (cur += next.project + ','));
+          return cur;
+        }, '');
+        this.getParentProjectList(data,"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzM4NDk5NzgsInVzZXJfbmFtZSI6Inp5IiwianRpIjoiMTZlNGE5ZGEtYWU5Mi00M2YwLTlmODYtYTNjNzI4YjE2ZDNiIiwiaWRlbnRpdHkiOiJ6eSIsImNsaWVudF9pZCI6IkNneXVuQ2xpZW50SWQiLCJzY29wZSI6WyJyZWFkIl19.iWD3z4ziP2RSMoa0jofRJkCO-dbbR-Kka7CJdMPbnNQ")
+      })
+    },
+    getParentProjectList(data,token) {
+
+      this.$http.post("http://newsightcgteam.cgyun.cn:18889/cgteam/cgteam/getRemoteProject",
+        { projects: data, token: token}).then(response=>{
+        const res = response.data
+        const list = res.data;
+        for (let i = 0; i < list.length; i++) {
+          this.projectList.push({
+            id: i,
+            name: list[i].name,
+            alias: list[i].name,
+            image: list[i].coverUrl,
+          });
+        }
+      });
+
+    }
+  }
 }
 </script>
 
